4a388439d84ab6207e552c47ba18f96ede3f00d3
unknown <uudquark@soongsil.ac.kr>
2021-08-21 21:49:14 +0900

    8/21
    OC1

---
+++ OpenChallenge1/src/MyInfoApp.java
@@ -0,0 +1,15 @@
+public class MyInfoApp {
+    public static void main(String[] args){
+        String name;
+        int age;
+        String major;
+
+        name = new String ("Kitae Hwang");
+        age= 20;
+        major = new String("Department of Computer Engineering");
+
+        System.out.println(name);
+        System.out.println(age+" years old");
+        System.out.println(major);
+    }
+}
